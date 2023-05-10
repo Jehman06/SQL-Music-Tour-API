5c6c452ac4da54d34edcd8f8f979f0de07679ed6
Jeremy Lehmann <jeremylehmann@Jeremys-MacBook-Pro.local>
2023-05-09 21:24:50 -0700

    Created the bands table using sequelize CLI

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { Sequelize } = require('sequelize');
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require('dotenv').config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: process.env.PG_PASSWORD
+});
 
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
-        message: 'Welcome to the Tour API.'
-    })
-})
+        message: 'Welcome to the Tour API!'
+    });
+});
 
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
